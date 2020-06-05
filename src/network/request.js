f04f77d00398c0117aff99350ee710a667c9849d
HQHollow <1354252799@qq.com>
2020-06-05 12:41:13 +0800

    shengsai2

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://47.107.99.96:5000',
+    baseURL: 'https://47.107.99.96:5000',
     timeout: 9000
   })
   return instance(config)
